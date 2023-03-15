4524ea03010d10f8beff6448bb032b4c51f2a073
José Miguel <101609455+Zezenta@users.noreply.github.com>
2023-03-14 23:08:53 -0500

    Primeros archivos guardados, nada funcional

---
+++ GaltonMachine.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <cstdlib> // contiene la funcion system("pause")
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, my name is Miguel Veliz, and you are now going to see a simple implementation of the Galton Machine." << endl;
+    cout << "But, what is a Galton Machine?, well, in few words, it is a bunch of marbles that have gone through a random process, but with an expected outcome." << endl;
+    cout << "Now, we are going to see the same process in c++." << endl;
+
+
+
+    system("pause");
+}
+
+void Machine()
+{
+    for (int i = 0; i < 230; i++)
+    {
+        /* code */
+    }
+    
+}
